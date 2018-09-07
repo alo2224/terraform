32c37347671564f2a2b7ed2be0c6b4208d5ddb13
EC2 Default User <ec2-user@ip-10-0-0-203.ec2.internal>
2018-09-07 13:09:27 +0000

    Additional changes

--- networking/main.tf
+++ networking/main.tf
@@ -44,7 +44,7 @@ resource "aws_subnet" "tf_public_subnet"{
     vpc_id = "${aws_vpc.tf_vpc.id}"
     cidr_block = "${var.public_cidrs[count.index]}"
     map_public_ip_on_lauch = true
-    availability_zone = "${data.aws_availability_zones.available_names[count.index]}"
+    availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
     
     tags {
         Name = "tf_public_${count.index + 1}"
@@ -53,14 +53,14 @@ resource "aws_subnet" "tf_public_subnet"{
 
 resource "aws_route_table_association" "tf_public_assoc"{
     count = "${aws_subnet.tf_public_subnet.count}"
-    subnet_id = "${aws_subnet.tf_public_subnet.*.id[count.index]"
+    subnet_id = "${aws_subnet.tf_public_subnet.*.id[count.index]}"
     route_table_id = "${aws_route_table.tf_public_rt.id}"
 }
 
 resource "aws_security_group" "tf_public_sg"{
     name = "tf_public_sg"
     description = "Used for access to the public instances"
-    vpc_id = "${aws_vc.tf_vpc.id}"
+    vpc_id = "${aws_vpc.tf_vpc.id}"
     
     #SSH
     ingress {
